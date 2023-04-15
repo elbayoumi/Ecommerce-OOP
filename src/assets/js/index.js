5b0de9d1b7f14e11bbbecfbec41dec5b40e86f65
elbayoumi <mohamedashrafelbayoumi.com>
2023-04-15 16:51:23 +0200

    v1.8.4

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -4,21 +4,36 @@ import CheckLogin from './controler/CheckLogin.js';
 import FetchApi from './view/FetchApi.js';
 const signOut = document.querySelector(".signOut")
 const seemore = document.querySelector(".seemore")
-
+const seeless = document.querySelector(".seeless")
 window.addEventListener("load", (event) => {
     console.log("page is fully loaded");
     FetchApi.fechApi(localStorage.getItem("pagenation"))
   });
-  localStorage.getItem("pagenation")>=20?seemore.style.display="none":seemore.style.display="block";
-  seemore.addEventListener("click",()=>{
-    let count=localStorage.getItem("pagenation")||5;
-    localStorage.removeItem("pagenation");
-    localStorage.setItem("pagenation",Number(count)+5);
-    // location.reload();
-    // console.log("sssssssss")
-    FetchApi.fechApi(localStorage.getItem("pagenation"));
 
-});
+  const pagination=(prop,opreator)=>{
+
+    return prop.addEventListener("click",()=>{
+        
+            let count=localStorage.getItem("pagenation")||5;
+
+            switch (opreator) {
+                case "+":count=Number(count)+5;break;
+                case "-":count=Number(count)-5;break;
+            };
+
+            localStorage.removeItem("pagenation");
+            localStorage.setItem("pagenation",count);
+            localStorage.getItem("pagenation")>=20?seemore.style.display="none":seemore.style.display="block";
+            // localStorage.getItem("pagenation")<=5?seemore.style.display="block":seemore.style.display="none";
+            // location.reload();
+            // console.log("sssssssss")
+            FetchApi.fechApi(localStorage.getItem("pagenation"));
+
+        });
+    
+  }  
+pagination(seemore,"+");
+pagination(seeless,"-");
 // HoldElement.allPassword.forEach((prop)=>{
 //     // prop.classList.add="password_none"
 //     prop.style.display="none";
